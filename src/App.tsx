cab352d3c3290b96bba632ac8ea98f7d55e1b7d6
Rajeev Pai <rajeevpai.code@gmail.com>
2023-04-02 19:35:47 +0530

    Create puzzle layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FC, useState } from 'react';
+
+import { decryptWith, solveMe } from './utils';
+import { CustomButton, Confetti } from './components';
+
 import './App.css';
 
-function App() {
+const App: FC = () => {
+  const [answer, setAnswer] = useState('');
+  const [didAttempt, setDidAttempt] = useState(false);
+
+  function revealHandler() {
+    const secretKey = solveMe();
+    const result = decryptWith(secretKey);
+    setAnswer(result);
+    setDidAttempt(true);
+  }
+
+  function showMessage() {
+    if (answer) {
+      return (
+        <Confetti>
+          <h2>Congrats! </h2>
+          <p className="main-message">
+            You got the: <span className="challenge">{answer} !!</span>
+          </p>
+          <p>Go to the Game master to find the activity!</p>
+        </Confetti>
+      );
+    }
+
+    return <p className="error">Opps! Try again!</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {didAttempt && showMessage()}
+
+      {!answer && (
+        <CustomButton clickHandler={revealHandler}>Reveal</CustomButton>
+      )}
     </div>
   );
-}
+};
 
 export default App;
